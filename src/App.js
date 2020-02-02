1b5f4dba622db5d097fd9161c9e7a7cf8b5cb1a3
alexjrrambo <alexjr.rambo@gmail.com>
2020-02-02 17:23:59 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
 
-function App() {
+function App () {
+  const [techs, setTech] = useState(['React', 'React Native'])
+  const [newTech, setNewTech] = useState('')
+
+  // useCallBack visa fazer com que a funcition só seja reescrita na memória
+  // caso houver alteração do state
+  const handleAdd = useCallback(() => {
+    setTech([...techs, newTech])
+    setNewTech('')
+  }, [newTech, techs])
+
+  // Nesse caso o use effect será disparado apenas na primeira instacia do componente
+  // componentDidMount
+  useEffect(() => {
+    // Simula componentWillUnmount
+    return () => {}
+  }, [])
+
+  // Monitarando techs e toda vez que alterado é disparado
+  useEffect(() => {
+
+  }, [techs])
+
+  // Deve ser utilizado quando temos calculos dentro do jsx, visando chamar
+  // apenas quando necessário
+  const techSize = useMemo(() => techs.length, [techs])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        {techs.map((tech) => (<li key={tech}>{tech}</li>))}
+      </ul>
+      <strong>You have {techSize} technologies</strong>
+      <br />
+      <input onChange={e => setNewTech(e.target.value)} />
+      <button onClick={handleAdd}>Add</button>
+    </>
+  )
 }
 
-export default App;
+export default App
